92ba6680a34199d03f96b2c12c3857dac17acd44
Daniel López <hyperdaniel579exe@gmail.com>
2022-12-09 17:55:07 -0600

    páginas, componentes y rutas actualizados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,32 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import {
+  Home,
+  Login,
+  Signup,
+  Profile,
+  Games,
+  NotFoundPage,
+} from "./pages/Index";
 
 function App() {
   return (
-    <>Hello world</>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="children-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ frontend/src/pages/Index.js
@@ -0,0 +1,6 @@
+export { Home } from "./Home";
+export { Login } from "./Login";
+export { Signup } from "./Signup";
+export { Profile } from "./Profile";
+export { Games } from "./Games";
+export { NotFoundPage } from "./NotFoundPage";
